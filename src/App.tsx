dfd53d18fd9ab3210cef3e5790676dbc4ab7dc07
Okeanos <76732468+Okeanos-0@users.noreply.github.com>
2022-01-21 14:54:50 +0300

    v

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App: FC = () => {
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/projects" element={<Projects />} />
+                <Route path="/okeanos/projects" element={<Projects />} />
             </Routes>
         </div>
     );
